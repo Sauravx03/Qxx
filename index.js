c18e5d545882fa7ece154148e229158bb76be9a7
Sauravghoshab <101920832+Sauravghoshab@users.noreply.github.com>
2022-03-20 01:52:44 +0530

    Added about reference

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var server = http.createServer((req,res)=>{
         res.write("Hello World ! /");
         res.end();
     }
+    if(req.url=="/about") 
+    {
+        res.writeHead(200,{"Content-Type":"text/plain"}); 
+        res.write("Hello World ! about"); 
+        res.end(); 
+    }
 });
 
 server.listen(PORT,()=>{
